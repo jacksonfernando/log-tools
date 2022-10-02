c03555ab00787dca155c98f6c62e7424cc2c478e
jacksonfernando <jacksonfernando51@gmail.com>
2022-10-02 11:11:56 +0700

    [Jackson] refactor parsing command

--- main.go
+++ main.go
@@ -11,15 +11,15 @@ type userInput struct {
 	target string
 }
 
-func main() {
-	filePath := os.Args[1]
-	var target string
-	var output string
+func parseCommand() (filepath string, target string, output string) {
+	filepath = os.Args[1]
 	mySet := flag.NewFlagSet("", flag.ExitOnError)
-  mySet.StringVar(&target, "t", "", "target of command")
-  mySet.StringVar(&output, "o", "", "output of command")
-  mySet.Parse(os.Args[2:])
-	fmt.Println(filePath)
-	fmt.Println(target)
-	fmt.Println(output)
+	mySet.StringVar(&target, "t", "", "target of command")
+	mySet.StringVar(&output, "o", "", "output of command")
+	mySet.Parse(os.Args[2:])
+	return filepath, target, output
+}
+func main() {
+	filepath, target, output := parseCommand()
+	fmt.Println(filepath, target, output)
 }
